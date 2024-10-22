be9e6a9411d2dd8110390187991631e81a688f7c
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2024-10-21 18:31:04 -0600

    feat: update geolocation model

--- db/migrate/20241021220713_create_geolocations.rb
+++ db/migrate/20241021220713_create_geolocations.rb
@@ -2,11 +2,31 @@ class CreateGeolocations < ActiveRecord::Migration[7.2]
   def change
     create_table :geolocations do |t|
       t.string :ip
+      t.string :input
       t.string :city
       t.string :country
       t.decimal :latitude
       t.decimal :longitude
       t.string :provider
+      t.string :type_of_ip
+      t.string :ip_version
+      t.string :continent_code
+      t.string :continent_name
+      t.string :country_code
+      t.string :country_name
+      t.string :region_code
+      t.string :region_name
+      t.string :zip
+      t.string :msa
+      t.string :dma
+      t.string :ip_routing_type
+      t.string :connection_type
+      t.integer :geoname_id
+      t.string :capital
+      t.string :country_flag
+      t.string :country_flag_emoji
+      t.string :calling_code
+      t.boolean :is_eu
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,31 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_21_220713) do
 
   create_table "geolocations", force: :cascade do |t|
     t.string "ip"
+    t.string "input"
     t.string "city"
     t.string "country"
     t.decimal "latitude"
     t.decimal "longitude"
     t.string "provider"
+    t.string "type_of_ip"
+    t.string "ip_version"
+    t.string "continent_code"
+    t.string "continent_name"
+    t.string "country_code"
+    t.string "country_name"
+    t.string "region_code"
+    t.string "region_name"
+    t.string "zip"
+    t.string "msa"
+    t.string "dma"
+    t.string "ip_routing_type"
+    t.string "connection_type"
+    t.integer "geoname_id"
+    t.string "capital"
+    t.string "country_flag"
+    t.string "country_flag_emoji"
+    t.string "calling_code"
+    t.boolean "is_eu"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
