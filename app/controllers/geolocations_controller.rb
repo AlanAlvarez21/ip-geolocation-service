3bacce901bcc38e67faa853ff68a796e81047065
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2024-10-21 18:30:41 -0600

    feat: update geolocation controller

--- app/controllers/geolocations_controller.rb
+++ app/controllers/geolocations_controller.rb
@@ -8,9 +8,36 @@ class GeolocationsController < ApplicationController
   
     def create
       begin
-        geolocation_data = GeolocationService.new(geolocation_params[:ip]).call
+        data = GeolocationService.new(geolocation_params[:input]).call
+
+        geolocation_data = {
+          input: geolocation_params[:input],
+          ip: data['ip'],
+          ip_version: data['type'],
+          continent_code: data['continent_code'],
+          continent_name: data['continent_name'],
+          country_code: data['country_code'],
+          country_name: data['country_name'],
+          region_code: data['region_code'],
+          region_name: data['region_name'],
+          city: data['city'],
+          zip: data['zip'],
+          latitude: data['latitude'],
+          longitude: data['longitude'],
+          msa: data['msa'],
+          dma: data['dma'],
+          ip_routing_type: data['ip_routing_type'],
+          connection_type: data['connection_type'],
+          geoname_id: data.dig('location', 'geoname_id'),
+          capital: data.dig('location', 'capital'),
+          country_flag: data.dig('location', 'country_flag'),
+          country_flag_emoji: data.dig('location', 'country_flag_emoji'),
+          calling_code: data.dig('location', 'calling_code'),
+          is_eu: data.dig('location', 'is_eu')
+        }
+
         @geolocation = Geolocation.create(geolocation_data)
-  
+
         if @geolocation.save
           render json: @geolocation, status: :created
         else
@@ -22,7 +49,16 @@ class GeolocationsController < ApplicationController
     end
   
     def show
-      render json: @geolocation
+      geolocation = Geolocation.find(params[:id])
+  
+      render json: geolocation
+
+      # render json: geolocation.as_json(only: [
+      #   :ip, :type_of_ip, :continent_code, :continent_name, :country_code, 
+      #   :country_name, :region_code, :region_name, :city, :zip, :latitude, 
+      #   :longitude, :msa, :dma, :ip_routing_type, :connection_type, :geoname_id, 
+      #   :capital, :country_flag, :country_flag_emoji, :calling_code, :is_eu
+      # ])
     end
   
     def destroy
@@ -39,7 +75,7 @@ class GeolocationsController < ApplicationController
     end
   
     def geolocation_params
-      params.require(:geolocation).permit(:ip)
+      params.require(:geolocation).permit(:input)
     end
   end
   
